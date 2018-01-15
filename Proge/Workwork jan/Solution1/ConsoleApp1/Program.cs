6adacdfb8dc76420c2920e404930467efd061aaf
Triin88 <33734540+Triin88@users.noreply.github.com>
2018-01-15 13:04:26 +0200

    Tunnikontroll

---
+++ Proge/Workwork jan/Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programm mis lubab sisestada N+1 hulga erinevaid sõnu, kuni sisestatakse -1 \n");
+
+            List<string> sõnad = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Sisesta sõna:");
+                string input = Console.ReadLine();
+                if (input == "-1")
+                {
+                    break;
+                }
+                else
+                {
+                    sõnad.Add(input);
+                }
+            }
+
+            Console.Write("Sisestasite sõnad: \n");
+            sõnad.ForEach(i => Console.Write("{0}\t \n", i));
+            Console.WriteLine("Valin nendest välja suvalise: ");
+            Random rnd = new Random();
+            int r = rnd.Next(sõnad.Count);
+            Console.WriteLine((string)sõnad[r]);
+
+
+            Console.ReadKey();
+        }
+    }
+}
